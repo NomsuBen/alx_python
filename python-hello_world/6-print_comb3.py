3db2f9c24a8e0d33d7c899875ed8d41723cf581d
NomsuBen <olaniranbenjamin@.com>
2023-07-19 19:15:30 +0100

    Updated solution to task 6

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,3 @@
-for tens_digit in range(1, 10):
-    for units_digit in range(0, tens_digit):
-        print("{:02d}".format(units_digit * 10 + tens_digit), end=", " if tens_digit > 2 else "\n")
+for tens_digit in range(10):
+    for units_digit in range(tens_digit+1, 10):
+        print("{:02d}".format(tens_digit * 10 + units_digit), end=", " if tens_digit < 8 else "\n")
