39ca08bd96a2ff05da1efb35caaeafba1a86e8b2
NomsuBen <olaniranbenjamin@gmail.com>
2023-07-19 08:09:20 -0700

    Solution to task 2 updated

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10, 10)
 print(number)
 if number > 0:
-    print("is positive")
+    print("if the number is greater than 0: is positive")
 elif number == 0:
-    print("is zero")
+    print("if the number is 0: is zero")
 else:
-    print("is negative")
+    print("if the number is less than 0: is negative")
