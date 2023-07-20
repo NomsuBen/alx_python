a123d08469a472f7a41670ff2ea4bf791feb0da8
NomsuBen <olaniranbenjamin@gmail.com>
2023-07-20 08:18:36 -0700

    Updated and deleted main files

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,8 +1,11 @@
 def pow(a, b):
     result = 1
+    if b < 0:
+        a = 1 / a
+        b = -b
     while b > 0:
-        if b & 1:
+        if b % 2 == 1:
             result *= a
         a *= a
-        b >>= 1
+        b //= 2
     return result
