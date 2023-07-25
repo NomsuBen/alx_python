79cd36ef16aff0dbaab283b452fcc92485cd8eaa
NomsuBen <olaniranbenjamin@.com>
2023-07-25 19:51:19 +0100

    Updated

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,17 +1,10 @@
 def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+    return a + b 
+if __name__ == "__main__":
+    pass
 
 a = 1
 b = 2
-__import__("0-add")
+from add_0 import add
 result = add(a, b)
 print("{} + {} = {}".format(a, b, result))
