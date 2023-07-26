50cda612e1c02a6c951647ad1c32530d735d0fdd
NomsuBen <olaniranbenjamin@.com>
2023-07-26 19:46:30 +0100

    Updated

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -5,6 +5,11 @@ def raise_exception_msg(message=""):
     raise CustomException(message)
 
 try:
-    raise_exception_msg("This is a custom exception message.")
+    raise_exception_msg("C is fun")
 except CustomException as e:
-    print("Exception raised:", str(e))
+    print(e)
+
+try:
+    raise_exception_msg("Python is cool")
+except CustomException as e:
+    print(e)
