77ba0125fe2422ea4aaedac3fcdb9639678c433e
NomsuBen <olaniranbenjamin@.com>
2023-07-26 20:02:31 +0100

    Solution to task 3

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,31 +1,9 @@
 #!/usr/bin/python3
-safe_print_division = __import__('3-safe_print_division').safe_print_division
-
 def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDivisionError:
-        print("Error: Cannot divide by zero!")
-        return None
-    except TypeError:
-        print("Error: Both inputs should be integers!")
-        return None
-    else:
-        print("Inside result: {}".format(result))
-        print("{} / {} = {}".format(a, b, result))
-        return result
+    except (ZeroDivisionError, TypeError)
+        result = None
     finally:
-        print("Finally: Division operation completed.")
-
-# Test the function
-a = 10
-b = 2
-safe_print_division(a, b)
-
-c = 12
-d = 0
-safe_print_division(c, d)
-
-e = "invalid"
-f = 2
-safe_print_division(e, f)
+        print("Inside result: {}".format(result))
+        return result
