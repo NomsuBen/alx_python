9a37d58ca58869c861eff153465d53fb49f749de
NomsuBen <olaniranbenjamin@.com>
2023-07-26 19:52:12 +0100

    Updated

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,15 +1,3 @@
-def raise_exception_msg(message=""):
-    class CustomException(Exception):
-        pass
-
-    raise CustomException(message)
-
-try:
-    raise_exception_msg("C is fun")
-except CustomException as e:
-    print(e)
-
-try:
-    raise_exception_msg("Python is cool")
-except CustomException as e:
-    print(e)
+#!/usr/bin/python3
+def raise_exception_msg(message="")
+    raise NameError(message)
