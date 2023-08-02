da7a397e3b6992bddd3f9ea1287b7c4822d721fa
NomsuBen <olaniranbenjamin@.com>
2023-08-02 16:55:11 +0100

    Updated

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -3,23 +3,19 @@
 Creates a Square class.
 """
 
-from typing import Any
 
 Rectangle = __import__('7-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """
-    Class Square that inherits from Rectangle (9-rectangle.py)
+    """class Square that inherits from Rectangle (9-rectangle.py)
     Private instance attribute size.
     Public method area().
     Inherits from Rectangle.
     """
 
-    def __init__(self, size: int) -> None:
-        """
-        Initializes a Square.
-
+    def __init__(self, size):
+        """Initializes a Square.
         Args:
             - size: size of the square
         """
@@ -28,12 +24,11 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.__size = size
 
-    def __str__(self) -> str:
+    def __str__(self):
         return super().__str__()
 
-    def area(self) -> int:
-        """
-        Computes the area of a Square instance.
+    def area(self):
+        """Computes the area of a Square instance.
         Overwrites the area() method from Rectangle.
         """
 
