e35e1e839c4f44df973e04491353a541850b8764
NomsuBen <olaniranbenjamin@.com>
2023-08-10 20:12:55 +0100

    Updated

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -5,15 +5,19 @@ body of the response (decoded in utf-8).
 If the HTTP status code is greater than or equal to 400, print:
 Error code: followed by the value of the HTTP status code.
 """
-import sys
-import requests
 
+import requests
+import sys
+
+def main():
+    url = sys.argv[1]
+    
+    response = requests.get(url)
+    print("Response body:")
+    print(response.text)
+    
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    response = requests.get(url)
-    body = response.text
-    if response.status_code >= 400:
-        print("Error code: {}".format(response.status_code))
-    else:
-        print(body)
+    main()
