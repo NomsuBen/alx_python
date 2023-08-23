d8506dc40195680c14db1ffe14c400b994e751e1
NomsuBen <olaniranbenjamin@.com>
2023-08-23 17:32:34 +0100

    Updated

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,8 +3,8 @@
 This script lists all states from the
 database `hbtn_0e_0_usa`.
 
-Your script should take 3 arguments: 
-mysql username, mysql password and database name 
+Your script should take 3 arguments:
+mysql username, mysql password and database name
 (no argument validation needed)
 """
 
