9f3afc8ec51fb4805e8f3476416a96088733ce94
NomsuBen <olaniranbenjamin@.com>
2023-08-27 15:08:17 +0100

    Task 1 solved

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#! usr/bin/python3
+'''
+    script that starts a Flask web application
+'''
+
+from flask import Flask
+
+"""
+Access to the flask and get
+from the Flask.
+"""
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(port="5000")
