bdaae546590945703b81be5d65e59a86e68063ae
NomsuBen <olaniranbenjamin@.com>
2023-10-05 18:34:14 +0100

    Updated

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,35 +3,38 @@
 Python script to export data in the CSV format.
 """
 
-import csv
 import requests
 import sys
+import csv
 
+def get_todo_progress(employee_id):
+    # Get employee details
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+    employee_name = user_data["name"]
+    employee_username = user_data["username"]
 
-def export_to_CSV(user_id):
-    employee_name = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    ).json()["name"]
-    tasks = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
-    ).json()
+    # Get TODO list for the employee
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
 
-    tasks_data = []
+    # Count completed tasks and total tasks
+    completed_tasks = [task for task in todos_data if task["completed"]
 
-    for task in tasks:
-        tasks_data.append(
-            [
-                str(user_id),
-                employee_name,
-                task["completed"],
-                task["title"],
-            ]
-        )
+    # Print the result
+    print(f"Employee {employee_name} is done with tasks ({len(completed_tasks)}/{len(todos_data)}):")
+    for task in completed_tasks:
+        print(f"\t{task['title']}")
 
-    with open(str(user_id) + ".csv", "w", encoding="UTF8", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(tasks_data)
-
+    # Export data to CSV
+    csv_filename = f"{employee_id}.csv"
+    with open(csv_filename, mode='w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        for task in completed_tasks:
+            writer.writerow([employee_id, employee_username, "Completed", task['title'])
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -40,6 +43,6 @@ if __name__ == "__main__":
 
     try:
         employee_id = int(sys.argv[1])
-        export_to_CSV(employee_id)
+        get_todo_progress(employee_id)
     except ValueError:
         print("Please provide a valid employee ID.")
